0496391706d333d2b2338c2b298afcc2d024cbcd
Gabriel Hipólito <hipolitodeveloper@gmail.com>
2025-06-03 22:52:00 -0300

    config: first structure

---
+++ app/(access)/index.tsx
@@ -0,0 +1,9 @@
+import SignIn from "../../components/ui/screens/access/signin";
+import {ThemedText} from "../../backup/ThemedText";
+
+const SignInScreen=  () => {
+
+    return <SignIn />
+}
+
+export default SignInScreen
---
+++ components/ui/screens/access/signin/index.tsx
@@ -0,0 +1,13 @@
+import {Text, View} from "react-native";
+import Screen from "../../../common/Screen";
+
+const SignIn = () => {
+
+    return (
+        <Screen>
+            <Text>Tees</Text>
+        </Screen>
+    )
+}
+
+export default SignIn;
